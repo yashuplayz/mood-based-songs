cd9172a633ce3cc7944e27750f3fe07bc762f8e8
Yashwanth K <167610728+yashuplayz@users.noreply.github.com>
2025-07-07 13:56:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CLIENT_ID = 'f5567173ea2343d5867eb33eae459285';
-const REDIRECT_URI = 'http://localhost:5500/callback.html';
+const REDIRECT_URI = 'https://yashuplayz.github.io/mood-based-songs/';
 const SCOPES = 'streaming user-read-email user-read-private user-modify-playback-state user-read-playback-state';
